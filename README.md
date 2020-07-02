53f24f8c76ec02d0211b1858ac86689959569d58
SeanZH <zhponly@gmail.com>
2020-07-02 18:14:05 +0800

    add

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # rustkid
 learn rust
-https://doc.rust-lang.org/stable/nomicon/
+[The Dark Arts of Unsafe Rust](https://doc.rust-lang.org/stable/nomicon/)
+[Rust By Example](https://doc.rust-lang.org/stable/rust-by-example/index.html)
+[The Rust Standard Library](https://doc.rust-lang.org/std/index.html)
---
+++ std/io/main.rs
@@ -0,0 +1,14 @@
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
+fn main() -> io::Result<()> {
+    let mut f = File::open("foo.txt")?;
+    let mut buffer = [0; 10];
+
+    // read up to 10 bytes
+    let n = f.read(&mut buffer)?;
+
+    println!("The bytes: {:?}", &buffer[..n]);
+    Ok(())
+}
