7ce48974d3f7733fe9aa6b933a0aefc52fd9f1f7
SeanZH <zhponly@gmail.com>
2020-09-11 18:26:54 +0800

    add web

--- rocket-web/src/main.rs
+++ rocket-web/src/main.rs
@@ -8,11 +8,14 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::request::Form;
+use rocket::http::{Header, Status};
+use rocket::request::{Form, FromRequest, Outcome};
 use rocket::response::content::Json;
 use rocket::Request;
+use rocket_contrib::serve::StaticFiles;
 use rocket_contrib::templates::Template;
 use serde::Serialize;
+use std::net::IpAddr;
 
 #[derive(Serialize)]
 struct Context {
@@ -51,6 +54,35 @@ fn not_found(req: &Request) -> String {
     format!("Oh no, We could not find the request path '{}'", req.uri())
 }
 
+#[derive(Responder)]
+struct MyResp<'my> {
+    body: String,
+    header: Header<'my>,
+}
+
+#[derive(Debug)]
+struct NginxRealIp(IpAddr);
+
+impl<'a, 'r> FromRequest<'a, 'r> for NginxRealIp {
+    type Error = ();
+    fn from_request(request: &Request<'r>) -> Outcome<Self, Self::Error> {
+        match request.real_ip() {
+            Some(ip) => Outcome::Success(NginxRealIp(ip)),
+            _ => Outcome::Failure((Status::from_code(401).unwrap(), ())),
+        }
+    }
+}
+
+#[get("/ip")]
+fn ip<'a>(ip: NginxRealIp) -> MyResp<'a> {
+    let my = MyResp {
+        body: format!("this is body"),
+        header: Header::new("X-C", "this is header"),
+    };
+    println!("{:?}", ip);
+    my
+}
+
 //     => Error: No matching routes for GET /favicon.ico image/avif.
 #[get("/")]
 fn index() -> Template {
@@ -64,8 +96,9 @@ fn index() -> Template {
 fn main() {
     rocket::ignite()
         .register(catchers![not_found])
-        .mount("/", routes![index])
         .mount("/api/v1", routes![hello, new_book])
+        .mount("/", StaticFiles::from("./root"))
+        .mount("/", routes![ip])
         .attach(Template::fairing())
         .launch();
 }
