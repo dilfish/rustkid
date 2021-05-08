d8c1f1a76c650d894014b9009afba13df6a1b381
SeanZH <i@dilfish.dev>
2021-05-08 11:42:48 +0800

    delete old code

--- byexample/inner/fn_list.rs
+++
@@ -1,5 +0,0 @@
-// sean at shanghai
-
-pub fn mod_fn() {
-    println!("this is from inner::mod_fn");
-}
--- std/net/main.rs
+++
@@ -1,7 +0,0 @@
-use std::net::Ipv4Addr;
-
-fn main() {
-    let localhost = Ipv4Addr::new(127, 0, 0, 1);
-    assert_eq!("127.0.0.1".parse(), Ok(localhost));
-    assert_eq!(localhost.is_loopback(), true);
-}
